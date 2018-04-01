d6a1d4ebd62b6c6b0273ae3271f0b2532f5c4914
Rohitraj1104 <rrrohitraj54@gmail.com>
2018-04-02 04:59:21 +0530

    simple math programm

--- simplemath.py
+++ simplemath.py
@@ -1,13 +1,13 @@
 #!/bin/bash
-j=input("enter first value")
-k=input("enter second value")
+j=input("enter first value==")
+k=input("enter second value==")
 
 
-def sum(j,k):
+def sum(j,k): #this is to add two number
   l= j+k
-  print("TOTAL   " +str(l))
+  print("\nTOTAL   " +str(l))
 
-def expo(j,k):
+def expo(j,k):  #this is a exponential function 
    r=1
    o=0
    while o< k:
@@ -15,5 +15,14 @@ def expo(j,k):
     o += 1
    return r
 
+def multiple(n,m):
+  if n and m== False:
+   print("are u stupid")
+  else:
+   mul=n*m
+  return mul
+
+
 sum(j,k)
-print("exponent " +str(expo(j,k)))
+print("\nexponent== " +str(expo(j,k)))
+print("\nmultiply of two number== " +str(multiple(j,k)) + "\n")
